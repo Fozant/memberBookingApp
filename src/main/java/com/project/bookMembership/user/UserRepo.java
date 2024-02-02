fe3df7e25660ed7b25142b9ea8a83328617e8f1c
Fozant <vrozh7@gmail.com>
2024-02-02 23:04:03 +0700

    add config jwt

---
+++ src/main/java/com/project/bookMembership/user/UserRepo.java
@@ -0,0 +1,9 @@
+package com.project.bookMembership.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
+
