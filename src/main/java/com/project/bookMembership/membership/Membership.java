7efb87b8c86ca0717770edaa2a1494dfd9035336
Fozant <vrozh7@gmail.com>
2024-02-03 15:27:45 +0700

    add membership model

---
+++ src/main/java/com/project/bookMembership/membership/Membership.java
@@ -0,0 +1,42 @@
+package com.project.bookMembership.membership;
+
+import java.util.Date;
+
+import com.project.bookMembership.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "membership")
+public class Membership {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idMember;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Long transactionId;
+
+
+    private Date startDate;
+    private Date endDate;
+    private Long price;
+    
+}
