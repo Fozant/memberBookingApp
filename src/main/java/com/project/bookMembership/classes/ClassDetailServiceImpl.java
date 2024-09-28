34dfe20dd5366c533ae6e9b949cfda621c2a401a
vrozh7 <vrozh7@gmail.com>
2024-09-28 22:10:01 +0700

    rapihin

--- src/main/java/com/project/bookMembership/classes/ClassDetailServiceImpl.java
+++ src/main/java/com/project/bookMembership/classes/ClassDetailServiceImpl.java
@@ -24,27 +24,6 @@ public class ClassDetailServiceImpl implements ClassDetailService {
         this.trainingClassRepo = trainingClassRepo;
     }
 
-//     @Override
-// public ClassDetail addClassDetail(ClassDetailRequest classDetailRequest) {
-//     // Extract username from the provided token
-//     String username = jwtService.extractUsername(classDetailRequest.getToken());
-
-//     // Retrieve the TrainingClass using the provided idClass
-//     Long idClass = classDetailRequest.getIdClass();
-//     TrainingClass trainingClass = classDetailRepository.findTrainingClassById(idClass)
-//             .orElseThrow(() -> new RuntimeException("TrainingClass not found"));
-
-//     // Retrieve the User using the extracted username
-//     User user = userRepo.findByEmail(username)
-//             .orElseThrow(() -> new RuntimeException("User not found"));
-
-//     var classDetail = ClassDetail.builder()
-//             .idUser(user) 
-//             .idClass(trainingClass)
-//             .build();
-
-//     return classDetailRepository.save(classDetail);
-// }
 
 
 @Override
@@ -55,7 +34,7 @@ public ClassDetail book(ClassDetailRequest classDetailRequest) {
        User user = userRepo.findByEmail(emailz)
        .orElseThrow(() -> new RuntimeException("User not found"));
 
-        // Check if the training class exists
+   
         Long idTrainingclass = classDetailRequest.getIdClass();
         TrainingClass trainingClass = trainingClassRepo.findById(idTrainingclass)
         .orElseThrow(() -> new RuntimeException("Training class not found"));
@@ -69,16 +48,6 @@ public ClassDetail book(ClassDetailRequest classDetailRequest) {
     return classDetailRepository.save(classDetail);
 
 }
-// @Override
-// public TrainingClass save(ClassRequest request) {
-//     var trainingClass = TrainingClass.builder()
-//         .classDate(request.getClassDate())
-//         .classTime(request.getClassTime())
-//         .classCapasity(request.getClassCapasity())
-       
-//         .build();
-      
-//     return trainingClassRepo.save(trainingClass);
-// }
+
 
 }
